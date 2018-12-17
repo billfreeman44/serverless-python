33997b9be521be09e04f23c013d4cabbf5fd2969
Evan Sosenko <razorx@evansosenko.com>
2018-12-16 19:12:55 -0800

    Use numpy in handler

--- handlers/todo.py
+++ handlers/todo.py
@@ -1,7 +1,9 @@
 import json
+import numpy as np
 
 def main(event, context):
+    a = np.arange(15).reshape(3, 5)
     return {
         'statusCode': 200,
-        'body': json.dumps({'data': 'TODO'})
+        'body': json.dumps({'data': a.tolist()})
     }
